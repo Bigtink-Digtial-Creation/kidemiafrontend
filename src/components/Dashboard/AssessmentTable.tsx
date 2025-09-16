458238c61578defb3ec199838a20a81826f1828e
sammy-code98 <sammysmart98@yahoo.com>
2025-09-16 13:29:21 +0100

    feat: dashboard ui and graphs

--- src/components/Dashboard/AssessmentTable.tsx
+++ src/components/Dashboard/AssessmentTable.tsx
@@ -1,23 +1,29 @@
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Chip } from "@heroui/react";
-import { mockTableData, type TableRowI } from '../../staticData';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Chip,
+} from "@heroui/react";
+import { mockTableData, type TableRowI } from "../../staticData";
 
-const statusTheme: Record<TableRowI["status"], string> = {
-  completed: "bg-kidemia-completed",
-  failed: "bg-kidemia-failed",
-  pending: "bg-kidemia-pending",
+const statusTheme: Record<
+  TableRowI["status"],
+  "success" | "warning" | "danger"
+> = {
+  completed: "success", // Maps to Chip's color prop
+  pending: "warning",
+  failed: "danger",
 };
 
 export default function AssessmentTable() {
-  
-  const getStatusClass = (status:TableRowI["status"]) => {
-    return statusTheme[status] || ""
-  }
-
   return (
     <div>
       <Table aria-label="Example static collection table">
         <TableHeader>
-          <TableColumn className='w-12'>S/N</TableColumn>
+          <TableColumn className="w-12">S/N</TableColumn>
           <TableColumn>Title</TableColumn>
           <TableColumn>Average Score (%)</TableColumn>
           <TableColumn>Status</TableColumn>
@@ -25,23 +31,31 @@ export default function AssessmentTable() {
           <TableColumn>Date Created</TableColumn>
           <TableColumn>Action</TableColumn>
         </TableHeader>
-       
+
         <TableBody>
           {mockTableData.map((mock) => (
             <TableRow key={mock.sn}>
-            <TableCell>{mock.sn}</TableCell>
-            <TableCell>{mock.title}</TableCell>
-            <TableCell>{mock.averageScore}</TableCell>
-            <TableCell>
-                <Chip className={`${getStatusClass(mock.status)} bg-red-500- text-xs px-3 capitalize`}>{mock.status}</Chip>
-            </TableCell>
-            <TableCell>{mock.comment}</TableCell>
-            <TableCell>{mock.dateCreated}</TableCell>
-            <TableCell className='text-xs text-kidemia-secondary hover:underline cursor-pointer'>View Result</TableCell>
-          </TableRow>
+              <TableCell>{mock.sn}</TableCell>
+              <TableCell>{mock.title}</TableCell>
+              <TableCell>{mock.averageScore}</TableCell>
+              <TableCell>
+                <Chip
+                  color={statusTheme[mock.status]}
+                  className="text-xs px-3 capitalize"
+                  variant="flat"
+                >
+                  {mock.status}
+                </Chip>
+              </TableCell>
+              <TableCell>{mock.comment}</TableCell>
+              <TableCell>{mock.dateCreated}</TableCell>
+              <TableCell className="text-xs text-kidemia-secondary hover:underline cursor-pointer">
+                View Result
+              </TableCell>
+            </TableRow>
           ))}
         </TableBody>
       </Table>
     </div>
-  )
+  );
 }
--- src/components/Dashboard/AssesstmentHistory.tsx
+++ src/components/Dashboard/AssesstmentHistory.tsx
@@ -1,26 +1,32 @@
-import { Tab, Tabs } from '@heroui/react'
-import AssessmentTable from './AssessmentTable'
+import { Tab, Tabs } from "@heroui/react";
+import AssessmentTable from "./AssessmentTable";
 
 export default function AssesstmentHistory() {
   return (
-    <div className='p-5 border border-kidemia-grey/15 rounded-2xl'>
+    <div className="p-5 border border-kidemia-grey/15 rounded-2xl">
       <div>
-      <Tabs aria-label='Assessment' size='sm' variant='underlined' fullWidth
-      classNames={{
-        tabList:"gap-6 w-full relative rounded-none p-0 border-b border-divider",
-        cursor:"w-full bg-[#BF4C20]",
-        tab:"max-w-fit- w-full px-0 h-10",
-        tabContent: "text-sm  group-data-[selected=true]:text-[#BF4C20] group-data-[selected=true]:font-semibold",        
-      }}
-      >
-        <Tab key="subjects" title="Subjects">
-            <AssessmentTable/>
-        </Tab>
-        <Tab key="topics" title="Topics">
-            <AssessmentTable/>
-        </Tab>
-      </Tabs>
+        <Tabs
+          aria-label="Assessment"
+          size="sm"
+          variant="underlined"
+          fullWidth
+          classNames={{
+            tabList:
+              "gap-6 w-full relative rounded-none p-0 border-b border-divider",
+            cursor: "w-full bg-[#BF4C20]",
+            tab: "max-w-fit- w-full px-0 h-10",
+            tabContent:
+              "text-sm  group-data-[selected=true]:text-[#BF4C20] group-data-[selected=true]:font-semibold",
+          }}
+        >
+          <Tab key="subjects" title="Subjects">
+            <AssessmentTable />
+          </Tab>
+          <Tab key="topics" title="Topics">
+            <AssessmentTable />
+          </Tab>
+        </Tabs>
       </div>
     </div>
-  )
+  );
 }
