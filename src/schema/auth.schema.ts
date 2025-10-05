86f574752fb776ef63d79a5df92a1db6def495fb
sammy-code98 <sammysmart98@yahoo.com>
2025-10-05 15:05:26 +0100

    login api call

--- src/schema/auth.schema.ts
+++ src/schema/auth.schema.ts
@@ -5,6 +5,7 @@ export const LoginSchema = z.object({
   password: z
     .string({ message: "Password is required" })
     .min(2, { message: "Password is required" }),
+  remember_me: z.boolean().default(false).optional(),
 });
 
 export const ForgotPasswordSchema = z.object({
