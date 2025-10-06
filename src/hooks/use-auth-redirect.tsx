26238eae48ac48ee55093a2cccddf4bed9e24962
sammy-code98 <sammysmart98@yahoo.com>
2025-10-06 16:01:13 +0100

    add route redirect hook

---
+++ src/hooks/use-auth-redirect.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+import { useAtom } from 'jotai'
+import { useNavigate } from 'react-router'
+import { jwtDecode } from "jwt-decode";
+import { loggedinUserAtom, storedAuthTokenAtom } from '../store/user.atom';
+import { AuthRoutes, SidebarRoutes } from '../routes';
+
+
+export function useAuthRedirect(requireAuth = true) {
+  const navigate = useNavigate()
+  const [loggedInUser, setLoggedInUser] = useAtom(loggedinUserAtom)
+  const [authToken, setAuthToken] = useAtom(storedAuthTokenAtom)
+
+  useEffect(() => {
+    if (!authToken) {
+      if (requireAuth) {
+        navigate(AuthRoutes.login, { replace: true })
+      }
+      return
+    }
+    try {
+      const decoded: { exp: number; iat: number } = jwtDecode(authToken)
+      const expireAt = decoded.exp * 1000;
+
+      if (expireAt < Date.now()) {
+        setAuthToken(null)
+        setLoggedInUser(null)
+        navigate(AuthRoutes.login, { replace: true })
+        return
+      }
+    } catch (err) {
+      console.error(err)
+      navigate(AuthRoutes.login, { replace: true })
+      return
+    }
+
+    if (requireAuth && !loggedInUser) {
+      navigate(AuthRoutes.login, { replace: true })
+    } else if (!requireAuth && loggedInUser) {
+      navigate(SidebarRoutes.dashboard, { replace: true })
+    }
+  }, [loggedInUser, authToken, navigate, requireAuth, setLoggedInUser, setAuthToken])
+  return { loggedInUser, authToken }
+}
