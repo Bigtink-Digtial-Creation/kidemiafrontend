8636037b3e98d443c446b017df863ad184a2fc44
samuel kufre willie <samuelkufrewillie@gmail.com>
2025-12-24 17:22:45 +0100

    bug: finding login bug2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero";
 import StatsAndTestimonial from "./components/Testimonial";
-import PricingAndHighlights from "./components/PricingAndHighlights";
+// import PricingAndHighlights from "./components/PricingAndHighlights";
 import PartnersSection from "./components/PartnersSection";
 import SubscribeBar from "./components/SubscribeBar";
 
